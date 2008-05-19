399844fae0da16a29dc0530c09070f3d566719a1
maro <marek.milek@gmail.com>
2008-05-19 23:14:14 +0200

    Add home controller to have main page

--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,7 @@ ActionController::Routing::Routes.draw do |map|
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
   # See how all your routes lay out with "rake routes"
-
+  map.root :controller => 'home'
   # Install the default routes as the lowest priority.
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
