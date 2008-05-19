0882eb398d5e2ce7611d95c881fe3d0265568ff5
U-PROPRIO\Simon <Simon@proprio.(none)>
2008-05-19 23:25:27 +0200

    Added fields address, description and phone_no to user migrations

--- db/migrate/024_create_users.rb
+++ db/migrate/024_create_users.rb
@@ -1,17 +1,14 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users, :force => true do |t|
-      t.string :login, :email, :type
-
+      t.string :login, :email, :type, :address, :phone_no, :description
       t.column :crypted_password,          :string, :limit => 40
       t.column :salt,                      :string, :limit => 40
       t.column :remember_token,            :string
       t.column :remember_token_expires_at, :datetime
-
       t.timestamps
     end
   end
-
   def self.down
     drop_table :users
   end
