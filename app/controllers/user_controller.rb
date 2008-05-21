987b4106e51f17ea681d43487661b208dfc0c5c6
Jakub Kuźma <qoobaa@gmail.com>
2008-05-21 13:59:44 +0200

    Changing password separated from editing profile. Added basic application layout.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -11,19 +11,34 @@ class UserController < ApplicationController
   end
 
   def update
-    @user = User.authenticate(params[:user][:login], params[:old_password])
-    if @user.nil?
-      @user = current_user
-      @user.errors.add :old_password
+    @user = current_user
+    params[:user][:password] = params[:user][:password_confirmation] = nil
+    if @user.update_attributes(params[:user])
+      flash[:notice] = 'User profile was successfully updated.'
+      redirect_to user_path
+    else
       render :action => 'edit'
-    else 
-      if @user.update_attributes(params[:user])
-        flash[:notice] = 'User profile was successfully updated.'
+    end
+  end
+
+  def change_password
+    @user = current_user
+  end
+
+  def change_password_update
+    @user = current_user
+    if User.authenticate(@user.login, params[:current_password])
+      @user.password = params[:new_password]
+      @user.password_confirmation = params[:new_password_confirmation]
+      if current_user.save
+        flash[:notice] = 'Password successfully updated.'
         redirect_to user_path
       else
-        @user.password = @user.password_confirmation = nil
-        render :action => 'edit'
+        render :action => 'change_password'
       end
+    else
+      flash[:error] = 'Current password is incorrect'
+      render:action => 'change_password'
     end
   end
 
