e37be8e29e701357d6a8c649fc55928c59ba4e4f
Jakub Kuźma <qoobaa@gmail.com>
2008-05-22 17:19:35 +0200

    Prepare to merge with Kowal - fixtures needed

---
+++ app/controllers/admin/dependencies_controller.rb
@@ -0,0 +1,56 @@
+class Admin::DependenciesController < ApplicationController
+  before_filter :admin_required
+
+  def index
+    @dependencies = Dependency.find(:all)
+  end
+
+#   def new
+#     @user = User.new
+#   end
+
+#   def edit
+#     @user = User.find(params[:id])
+#   end
+
+#   def create
+#     @user = User.new(params[:user])
+
+#     @user.type = params[:user][:type]
+    
+#     if @user.save
+#       redirect_to admin_user_path(@user)
+#       flash[:notice] = "Thanks for signing up!"
+#     else
+#       @user.password = @user.password_confirmation = nil
+#       render :action => 'new'
+#     end
+#   end
+
+#   def update
+#     @user = User.find(params[:id])
+
+#     @user.type = params[:user][:type]
+
+#     if @user.update_attributes(params[:user])
+#       flash[:notice] = 'User was successfully updated.'
+#       redirect_to admin_user_path(@user)
+#     else
+#       @user.password = @user.password_confirmation = nil
+#       render :action => 'edit'
+#     end
+#   end
+
+   def show
+     @dependency = Dependency.find(params[:id])
+   end
+
+#   def destroy
+#     @user = User.find(params[:id])
+#     @user.destroy
+
+#     redirect_to(admin_users_url)
+#   end
+
+  
+end
