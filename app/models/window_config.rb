50e8d3333472fe917d77a9f7c4e17b2121a59258
Jakub Kuźma <qoobaa@gmail.com>
2008-05-24 23:33:34 +0200

    Validators for window_config model.

--- app/models/window_config.rb
+++ app/models/window_config.rb
@@ -4,12 +4,14 @@ class WindowConfig < ActiveRecord::Base
   has_many :additional_features, :through => :additional_feature_notes
   has_many :shutter_configs
   has_many :shutter_types, :through => :shutter_configs
-  belongs_to :customer, :class_name => 'User'
-
-	belongs_to :glass_type
-  belongs_to :glass_color
-	belongs_to :sash_structure
-	belongs_to :handle_type
   has_many :order_items, :as => :item
+  belongs_to :customer, :class_name => 'User'
+  belongs_to :glass_type
+  belongs_to :glass_color
+  belongs_to :sash_structure
+  belongs_to :handle_type
+
+  validates_presence_of :customer, :glass_type, :glass_color, :sash_structure, :handle_type
+  validates_numericality_of :height, :width, :only_integer => true, :greater_than => 0
 end
 
