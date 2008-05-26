cf0e23ee21d2e1b52ad565663d82e23e35d89f26
Jakub Kuźma <qoobaa@gmail.com>
2008-05-26 18:16:38 +0200

    Index in EventController for seller.

--- app/controllers/seller/events_controller.rb
+++ app/controllers/seller/events_controller.rb
@@ -1,2 +1,11 @@
 class Seller::EventsController < ApplicationController
+  def index
+    month = params[:m] || Date.today.month
+    year = params[:y] || Date.today.year
+    day = params[:d] || Date.today.day
+    @date = "#{year}-#{month}-#{day}".to_date
+    @month_events = Event.find_all_in_month(@date)
+    @due_events = Event.find_due_on_day(@date)
+    @added_events = Event.find_added_on_day(@date)
+  end
 end
--- app/helpers/seller/events_helper.rb
+++ app/helpers/seller/events_helper.rb
@@ -1,2 +1,26 @@
 module Seller::EventsHelper
+
+  def link_to_date(str, date)
+    link_to(str, { :y => date.year, :m => date.month, :d => date.day})
+  end
+
+  def month_name(date)
+    Date::MONTHNAMES[date.month]
+  end
+
+  def events_calendar(date, events)
+    calendar :month => date.month,
+             :year => date.year, 
+             :previous_month_text => link_to_date('<', date << 1),
+             :next_month_text => link_to_date('>', date >> 1) do |d|
+      has_event = false
+      events.each { |e| has_event = true if e.added_date.to_date == d || e.due_date.to_date == d }
+      if has_event
+        [link_to_date(d.day, d), { :class => 'hasEvent' }]
+      else
+        [link_to_date(d.day, d), { :class => 'noEvent' }]
+      end
+    end
+  end
+
 end
