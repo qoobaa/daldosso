afbbc44baf5b1d1e79e7ed554bd7c42c1b42e6bc
maro <marek.milek@gmail.com>
2008-05-30 13:24:38 +0200

    Glass size calculation with given constants and solved problems

--- app/models/window_config.rb
+++ app/models/window_config.rb
@@ -45,7 +45,7 @@ class WindowConfig < ActiveRecord::Base
   end
 
   def window_size
-    size = window_config.height * window_config.width * 1.0 # square cm
+    size = height * width * 1.0 # square cm
     size = size / 10000.0 # square m
     return size
   end
@@ -54,19 +54,19 @@ class WindowConfig < ActiveRecord::Base
     code = sash_structure.code # "F" or "PF"
     sashes = sash_structure.sashes_number # "sashes number"
     rail = height_medium_rail # if greater than zero then split window
-    frame_sash_width = 1.0
-    medium_rail_width = 1.0
-    medium_rail_height = 1.0
-    added_bottom_rail_width = 1.0
-    central_stiles = 1.0
+    frame_sash_width = 100
+    medium_rail_width = 80
+    medium_rail_height = height_medium_rail
+    added_bottom_rail_width = 70
+    central_stiles = 116
     h = height
     w = width
-    min_area = 0,5 * 1000000
+    min_area =  500000
 
     case code
         when SashStructure.code_f
           case sashes
-            when 0,1 # CODE F0,F1
+            when 1 # CODE F0,F1
               if rail > 0
                 hg1 = medium_rail_height - frame_sash_width
                 hg2 = h - ( medium_rail_width + frame_sash_width + medium_rail_height )
@@ -75,47 +75,46 @@ class WindowConfig < ActiveRecord::Base
                 area2 = hg2 * wg12
                 area1 = area1 > min_area ? area1 : min_area
                 area2 = area2 > min_area ? area2 : min_area
-                return area1 + area2
+                area = area1 + area2
               else
                 hg = h - ( 2 * frame_sash_width )
                 wg = w - ( 2 * frame_sash_width )
                 area = hg * wg
                 area = area > min_area ? area : min_area
-                return area
               end
             when 2 # CODE F2
               if rail > 0
                 hg1 = medium_rail_height - frame_sash_width
                 hg2 = h - ( medium_rail_width + frame_sash_width + medium_rail_height )
-                wg12 = ( w - [ ( 2 * frame_sash_width ) + central_stiles ] ) / 2.0
+                wg12 = ( w - ( ( 2 * frame_sash_width ) + central_stiles ) ) / 2.0
                 area1 = hg1 * wg12
                 area2 = hg2 * wg12
                 area1 = area1 > min_area ? area1 : min_area
                 area2 = area2 > min_area ? area2 : min_area
-                return (area1 + area2) * 2
+                area = (area1 + area2) * 2
               else
                 hg12 = h - ( 2 * frame_sash_width )
-                wg12 = ( w - [ ( 2 * frame_sash_width ) + central_stiles ] ) / 2.0
+                wg12 = ( w - ( ( 2 * frame_sash_width ) + central_stiles ) ) / 2.0
                 area = hg12 * wg12
                 area = area > min_area ? area : min_area
-                return area * 2
+                area = area * 2
               end
             when 3 # CODE F3
               if rail > 0
                 hg1 = medium_rail_height - frame_sash_width
                 hg2 = h - ( medium_rail_width + frame_sash_width + medium_rail_height )
-                wg123 = ( w - [ ( 2 * frame_sash_width ) + ( 2 * central_stiles ) ] ) / 3.0
+                wg123 = ( w - ( ( 2 * frame_sash_width ) + ( 2 * central_stiles ) ) ) / 3.0
                 area1 = hg1 * wg123
                 area2 = hg2 * wg123
                 area1 = area1 > min_area ? area1 : min_area
                 area2 = area2 > min_area ? area2 : min_area
-                return (area1+area2) * 3
+                area = (area1+area2) * 3
               else
                 hg123 = h - ( 2 * frame_sash_width )
-                wg123 = ( w - [ ( 2 * frame_sash_width ) + ( 2 * central_stiles ) ] ) / 3.0
+                wg123 = ( w - ( ( 2 * frame_sash_width ) + ( 2 * central_stiles ) ) ) / 3.0
                 area = hg123 * wg123
                 area = area > min_area ? area : min_area
-                return area * 3
+                area = area * 3
               end
             when 4 # CODE F4
               if rail > 0
@@ -126,18 +125,18 @@ class WindowConfig < ActiveRecord::Base
                 area2 = hg2 * wg1234
                 area1 = area1 > min_area ? area1 : min_area
                 area2 = area2 > min_area ? area2 : min_area
-                return (area1+area2) * 4
+                area = (area1+area2) * 4
               else
                 hg1234 = h - ( 2 * frame_sash_width )
                 wg1234 = (w - ( ( 2 * frame_sash_width ) + ( 3 * central_stiles ) ) ) / 4.0
                 area = hg1234 * wg1234
                 area = area > min_area ? area : min_area
-                return area * 4
+                area = area * 4
               end
           end
         when SashStructure.code_pf
           case sashes
-            when 0,1 # CODE PF1
+            when 1 # CODE PF1
               if rail > 0
                 hg1 = medium_rail_height - frame_sash_width - added_bottom_rail_width
                 hg2 = h - ( medium_rail_width + frame_sash_width + medium_rail_height )
@@ -146,13 +145,12 @@ class WindowConfig < ActiveRecord::Base
                 area2 = hg2 * wg12
                 area1 = area1 > min_area ? area1 : min_area
                 area2 = area2 > min_area ? area2 : min_area
-                return area1 + area2
+                area = area1 + area2
               else
                 hg = h - ( ( 2 * frame_sash_width ) - added_bottom_rail_width )
                 wg = w - ( 2 * frame_sash_width )
                 area = hg * wg
                 area = area > min_area ? area : min_area
-                return area
               end
             when 2 # CODE PF2
               if rail > 0
@@ -163,13 +161,13 @@ class WindowConfig < ActiveRecord::Base
                 area2 = hg2 * wg12
                 area1 = area1 > min_area ? area1 : min_area
                 area2 = area2 > min_area ? area2 : min_area
-                return (area1+area2) * 2
+                area = (area1+area2) * 2
               else
                 hg12 = h - (( 2 * frame_sash_width ) - added_bottom_rail_width )
                 wg12 = ( w - (( 2 * frame_sash_width ) + central_stiles ) ) / 2.0
                 area = hg12 * wg12
                 area = area > min_area ? area : min_area
-                return area * 2
+                area = area * 2
               end
             when 3 # CODE PF3
               if rail > 0
@@ -180,13 +178,13 @@ class WindowConfig < ActiveRecord::Base
                 area2 = hg2 * wg123
                 area1 = area1 > min_area ? area1 : min_area
                 area2 = area2 > min_area ? area2 : min_area
-                return (area1 + area2) * 3
+                area = (area1 + area2) * 3
               else
                 hg123 = h - ( ( 2 * frame_sash_width ) - added_bottom_rail_width )
                 wg123 = ( w - ( ( 2 * frame_sash_width ) + ( 2 * central_stiles ) )) / 3.0
                 area = hg123 * wg123
                 area = area > min_area ? area : min_area
-                return area * 3
+                area = area * 3
               end
             when 4 # CODE PF4
               if rail > 0
@@ -197,16 +195,18 @@ class WindowConfig < ActiveRecord::Base
                 area2 = hg2 * wg1234
                 area1 = area1 > min_area ? area1 : min_area
                 area2 = area2 > min_area ? area2 : min_area
-                return (area1 + area2) * 4
+                area = (area1 + area2) * 4
                else
                 hg1234 = h - ( ( 2 * frame_sash_width ) - added_bottom_rail_width)
                 wg1234 = ( w - ( ( 2 * frame_sash_width ) + ( 3 * central_stiles ) )) / 4.0
                 area = hg1234 * wg1234
                 area = area > min_area ? area : min_area
-                return area * 4
+                area = area * 4
               end
           end
-      end
+    end
+
+    return area / 1000000.0
   end
 
 end
